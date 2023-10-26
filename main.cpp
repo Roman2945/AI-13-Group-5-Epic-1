b7bdb51c6ddebc1b6be7bc7cf90a4a5eba972a33
roman_mydzhyn <Roman.Mydzhyn.ShI.2023@lpnu.ua>
2023-10-26 22:59:30 +0300

    Main commit

--- main.cpp
+++ main.cpp
@@ -6,15 +6,10 @@ int main()
 	cout << "Roman Mydzhyn" << endl;
 
 	cout << "Sviatoslav Humeniuk" << endl;
-		
-  cout << "Maksym Zolotarchuk" << endl;
+
+	cout << "Maksym Zolotarchuk" << endl;
 
 	cout << "Daria Fesenko" << endl;
 
-
-	
-	
-	
-	
 	return 0;
 }
