43e865f2ceccefa465c5cf445f7795a7625fafca
YuraSirenk <yurii.sirenko.shi.2023@lpnu.ua>
2023-11-10 21:47:50 +0200

    Epic2 maksymzolotarchuk first commit

---
+++ Epic2/Code/Kravatki.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int kravatki;
+    int man;
+    cin>>kravatki>>man;
+
+    int collected[kravatki];
+    for(int i = 0;i<kravatki;i++)
+    {
+        cin>>collected[i];
+    }
+
+}
